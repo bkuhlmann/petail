0ff990a8fdf276f158eab950dcc103b75b2bb4c8
Brooke Kuhlmann <brooke@alchemists.io>
2025-04-04 15:45:09 -0600

    Added public Object API
    Necessary to provides the primary interface for all use.

--- lib/petail.rb
+++ lib/petail.rb
@@ -6,4 +6,17 @@ require "petail/xml/serializer"
 
 # Main namespace.
 module Petail
+  MEDIA_TYPE_JSON = "application/problem+json"
+  MEDIA_TYPE_XML = "application/problem+xml"
+  TYPES = %i[json xml].freeze
+
+  def self.from_json(...) = Payload.from_json(...)
+
+  def self.from_xml(...) = Payload.from_xml(...)
+
+  def self.media_type_for key, types: TYPES
+    types.include?(key) ? const_get("MEDIA_TYPE_#{key.upcase}") : ""
+  end
+
+  def self.new(**) = Payload.for(**)
 end
